02047db00d6736853f91d903d7bdc0e148789e89
Colonel_Kristoffy <liam@kristoffy.com>
2024-12-16 11:16:30 +0000

    Adding user to Dockerfile

--- DeploymentProject/app/app.py
+++ DeploymentProject/app/app.py
@@ -13,12 +13,17 @@ def predict():
         if not request.is_json:
             return jsonify({"error": "Request content-type must be application/json"}), 415
         
+        
         # Extract data from request
         input_data = request.json.get("data")
-        
+
         if input_data is None:
             return jsonify({"error": "JSON must include 'data' key"}), 400
-        
+
+
+        if not isinstance(input_data, list) or not isinstance(input_data[0], list) or not all((isinstance(x, (int, float)) for x in input_data[i]) for i in range(len(input_data))):
+            return jsonify({"error": "Invalid data format. Must be a 2D list of numbers."}), 400
+
         # Prediction
         price_pred = pipeline.predict(input_data)
         
@@ -27,4 +32,4 @@ def predict():
         return jsonify({"error": str(e)}), 400
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=9999)
+    app.run(host="0.0.0.0", port=9999, debug=False)
